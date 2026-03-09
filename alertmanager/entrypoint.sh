5f41a9f8e8b5ccc74e26945fb14a56a9da214ad4
Anton Satt <makeorbreak+github@pm.me>
2026-03-09 01:34:32 +0100

    fix discord notification

--- alertmanager/entrypoint.sh
+++ alertmanager/entrypoint.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
-# Substitute environment variables in the config template using sed
-# (envsubst is not available in the alertmanager image)
-sed \
-  -e "s|\${SLACK_WEBHOOK_URL}|${SLACK_WEBHOOK_URL}|g" \
-  -e "s|\${SLACK_CHANNEL}|${SLACK_CHANNEL}|g" \
-  -e "s|\${SMTP_SMARTHOST}|${SMTP_SMARTHOST}|g" \
-  -e "s|\${SMTP_FROM}|${SMTP_FROM}|g" \
-  -e "s|\${SMTP_AUTH_USERNAME}|${SMTP_AUTH_USERNAME}|g" \
-  -e "s|\${SMTP_AUTH_PASSWORD}|${SMTP_AUTH_PASSWORD}|g" \
-  -e "s|\${ALERT_EMAIL_TO}|${ALERT_EMAIL_TO}|g" \
-  -e "s|\${GENERIC_WEBHOOK_URL}|${GENERIC_WEBHOOK_URL}|g" \
-  /etc/alertmanager/alertmanager.yml.tmpl > /etc/alertmanager/alertmanager.yml
-
-# Start alertmanager with all passed arguments
+cp /etc/alertmanager/alertmanager.yml.tmpl /etc/alertmanager/alertmanager.yml
 exec /bin/alertmanager "$@"
